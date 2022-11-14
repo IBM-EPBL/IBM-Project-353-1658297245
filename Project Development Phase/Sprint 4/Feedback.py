951ae74d29dd7488797826ead6561c8acd38f179
thiyagarajan2002 <trj08012002@gmail.com>
2022-11-14 20:11:15 +0530

    sprint 4

--- Project Development Phase/Sprint 4/Feedback.py
+++ Project Development Phase/Sprint 4/Feedback.py
@@ -1,19 +1,14 @@
-import mysql.connector
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+login = credentials.Certificate("key.json")
 
-
-def submit(name,email,phone_number,message):
-   mydb = mysql.connector.connect(host="localhost",user="root",password="2002",database="submit")
-   mycursor = mydb.cursor()
-   sql = "INSERT INTO customers (Name,Email,Phone_number,Feedback) VALUES (%s, %s,%s,%s)"
-   val = (name,email,phone_number,message)
-   mycursor.execute(sql, val)
-   mydb.commit()
-
-"""
-CREATE DATABASE submit;
-USE submit;
-CREATE TABLE submit(ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,Name varchar(30),Email varchar(40),Phone_number varchar(20),Feedback varchar(300));
-INSERT INTO submit(Name, Email, Phone_number, Feedback) VALUES ('trj', 'trj08012002@gmail.com', '6369671812', 'Add a live virutal assistance on chatbot');
-show tables;
-select * from submit;
-"""
+def submit(name,email_id,phone_number,message):
+   db=firestore.client()
+  feedback=db.collection("Feedback")
+  feedback.document().set({
+    'Name': name,
+    'Phone Number': phone_number,
+    'Email Id':email_id,
+    'Feedback': message
+    })
--- Project Development Phase/Sprint 4/sql.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE submit;
-USE submit;
-CREATE TABLE submit(ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,Name varchar(30),Email varchar(40),Phone_number varchar(20),Feedback varchar(300));
